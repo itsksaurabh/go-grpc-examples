a76f32581e94a4bd0ee96c49f4a9126235ff993e
Kumar Saurabh <kumarsaurabh@Kumars-MacBook-Air.local>
2020-04-13 12:51:25 +0530

    add client.go

---
+++ unary/sum/sumClient/client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/itsksaurabh/udemy/grpc/unary/sum/sumpb"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+
+	defer conn.Close()
+	c := sumpb.NewSumClient(conn)
+
+	num := sumpb.Numbers{
+		A: 10,
+		B: 5,
+	}
+
+	res, err := c.Add(context.Background(), &sumpb.SumRequest{Numbers: &num})
+	if err != nil {
+		log.Fatalf("failed to call Add: %v", err)
+	}
+	fmt.Println(res.Result)
+}
