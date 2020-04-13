cbefda8d6fbd7320d644bf5c69a45e34d28064a0
Kumar Saurabh <kumarsaurabh@Kumars-MacBook-Air.local>
2020-04-13 16:49:02 +0530

    add stream server.go

---
+++ stream/countdown/countdownServer/server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net"
+	"time"
+
+	"github.com/itsksaurabh/udemy/grpc/stream/countdown/countdownpb"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	countdownpb.RegisterCountDownServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+// Start starts countdown
+func (*server) Start(req *countdownpb.CountdownRequest, stream countdownpb.CountDown_StartServer) error {
+	t := req.GetTimer()
+	for t > 0 {
+		res := countdownpb.CountDownResponse{Count: t}
+		stream.Send(&res)
+		t--
+		time.Sleep(time.Second)
+	}
+	return nil
+}
